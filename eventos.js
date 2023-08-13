8e08bbfaf75a96b22af7fda4e8b98673ded4376d
caroantunez <caro.antunez2007@gmail.com>
2023-08-13 15:26:14 -0300

    Agregado manejador de eventos para el div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const saludarDiv = document.getElementById("saludarDiv");
+
+    saludarDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
